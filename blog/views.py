0bb3d0d0147887b5eb81b432e4cea1820b8b28ce
AlekseyAgaltsov <AlekseyAgaltsov@gmail.com>
2023-05-16 13:45:46 +0300

    100

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Постраничная разбивка с 3 постами на страницу
+    paginator = Paginator(post_list, 1)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
@@ -22,3 +32,10 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+class PostListView(ListView): # Альтернативное представление списка постов
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 1
+    template_name = 'blog/post/list.html'
